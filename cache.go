116be9fa1060ecaaac13bb0dab57b7cc181b2f89
conamu <constantin.amundsen@ho-me.zone>
2025-04-02 22:47:03 +0200

    revert unwanted changes

--- cache.go
+++ cache.go
@@ -74,7 +74,7 @@ func WithAutomaticQueryPlanCache() Option {
 }
 
 type queryPlanCacheItem struct {
-	LastUsed atomic.Value // time.Time `redis`
+	LastUsed atomic.Value // time.Time
 	Value    QueryPlanList
 }
 
